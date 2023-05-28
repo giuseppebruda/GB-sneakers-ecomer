911a1bdd1588398fd0860ade27c78eb2378f0697
giuseppe bruda <giuseppebruda10@gmail.com>
2023-05-28 15:41:14 -0300

    basico

---
+++ js/objet.js
@@ -0,0 +1,23 @@
+let productos = [
+//adidas
+    {id : 1 , nombre : "Adidas Forum", precio : 35000 , img :""  }
+    {id : 2 , nombre : "Adidas Ozalia", precio : 45000, imagen :  }
+    {id : 3 , nombre : "Adidas Shark", precio : 50000, imagen :  }
+    {id : 4 , nombre : "Yeezy 700", precio : 80000, imagen :  }
+    {id : 5 , nombre : "Yeezy 500", precio : 50000, imagen :  }
+    {id : 6 , nombre : " Adidas Adifom", precio : 75000, imagen :  }
+//nike
+    {id : 7 , nombre : "Nike air jordan retro 6", precio : , imagen :  }
+    {id : 8 , nombre : "Nike air more uptempo", precio : , imagen :  }
+    {id : 9 , nombre : "nike dunk loww", precio : , imagen :  }
+    {id : 10 , nombre : "Nike kobe proto 1", precio : , imagen :  }
+    {id : 11 , nombre : "Nike air jordan 4 (edicion : travis scott", precio : , imagen :  }
+    {id : 12 , nombre : "Nike air jordan retro 4", precio : , imagen :  }
+//fila
+    {id : 13 , nombre : "Fila D-formation", precio : , imagen :  }
+    {id : 14 , nombre : "Fila Distruptor new", precio : , imagen :  }
+    {id : 15 , nombre : "Fila Distructor 97", precio : , imagen :  }
+    {id : 16 , nombre : "Fila Grant hill", precio : , imagen :  }
+    {id : 17 , nombre : "Fila kj7", precio : , imagen :  }
+    {id : 18 , nombre : "Fila Venon", precio : , imagen :  }
+]
