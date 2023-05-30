3e8fdcefcea3c17293975635cf4d670573fdf251
giuseppe bruda <giuseppebruda10@gmail.com>
2023-05-29 23:05:37 -0300

    terceraPreEntrega

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
 let padreProductos = document.getElementById("padreProductos");
 let openCar = document.getElementById("openCar"); 
+let carritoContainer = document.getElementById("carritoContainer")
 
-//array con todos mis objetos 
-let productos = [
-    //adidas
-        {id : 1 , nombre : "Adidas Forum", precio : 35000 , imagen :"./img/adidas/adidas forum.jpg"  },
-        {id : 2 , nombre : "Adidas Ozalia", precio : 45000, imagen : "./img/adidas/adidas ozalia.jpg" },
-        {id : 3 , nombre : "Adidas Shark", precio : 50000, imagen : "./img/adidas/adidas shark.jpg" },
-        {id : 4 , nombre : "Yeezy 700", precio : 80000, imagen : "./img/adidas/adidas yeezy 700.png" },
-        {id : 5 , nombre : "Yeezy 500", precio : 50000, imagen : "./img/adidas/adidas yezzy 500.jpg" },
-        {id : 6 , nombre : " Adidas Adifom", precio : 75000, imagen : "./img/adidas/Zapatillas_adiFOM.jpg" },
-    //nike
-        {id : 7 , nombre : "Nike air jordan retro 6", precio : 80000 , imagen : "./img/nike/air jordan 6.jpg" },
-        {id : 8 , nombre : "Nike air more uptempo", precio :75000 , imagen : "./img/nike/nike air more uptempo.jpg" },
-        {id : 9 , nombre : "nike dunk loww", precio : 60000, imagen : "./img/nike/nike dunk low.jpg" },
-        {id : 10 , nombre : "Nike kobe proto 1", precio :47000 , imagen : "./img/nike/nike kobe 1 proto.jpg" },
-        {id : 11 , nombre : "Nike air jordan 4 (travis scott)", precio :60000 , imagen : "./img/nike/nikejordarn4 travis scot.jpg" },
-        {id : 12 , nombre : "Nike air jordan retro 4", precio : 78000, imagen : "./img/nike/retro4..jpg" },
-    //fila
-        {id : 13 , nombre : "Fila D-formation", precio :25000 , imagen : "./img/fila/fila D-formation.jpg" },
-        {id : 14 , nombre : "Fila Distruptor new", precio : 90000, imagen : "./img/fila/fila disruptor.jpg" },
-        {id : 15 , nombre : "Fila Distructor 97", precio :30000 , imagen : "./img/fila/fila distruptor 97.jpg" },
-        {id : 16 , nombre : "Fila Grant hill", precio : 44000, imagen : "./img/fila/fila grant hill 1.png" },
-        {id : 17 , nombre : "Fila kj7", precio :73000 , imagen : "./img/fila/fila kj7.jpg" },
-        {id : 18 , nombre : "Fila Venon", precio :120000 , imagen : "./img/fila/fila venon.jpg" },
-    ]
 //primero crear el array para generar el carrito
 let carrito = [];
+carrito = (localStorage.getItem("carrito")) ? JSON.parse(localStorage.getItem("carrito")) : [];
 
 // crear y mostrar los productos 
 const verProductos = () =>{
@@ -39,9 +17,74 @@ const verProductos = () =>{
         <h3 class="card__nombre">${producto.nombre}</h3>
         <p class="card__precio"> ${producto.precio} </p>
     </div>
-    <button id="card__btn">comprar</button>
+    <button id="cardBtn${producto.id}">comprar</button>
     `;
-        padreProductos.appendChild(cardContainer);
+        padreProductos.appendChild(cardContainer)
+    // Comprar
+    let comprar = document.getElementById(`cardBtn${producto.id}`)
+    comprar.addEventListener("click", ()=>{
+        sumarAlCarrito(producto.id);
+        console.log(`elegido ${producto.nombre}`);
     })
+    });
+};
+verProductos();
+//creando la funcion que me agrega los productos al carrito con el uso del id 
+const sumarAlCarrito = (id) => {
+    let productoEnCarrito = carrito.find((producto) =>producto.id === id);
+    if (productoEnCarrito){
+        productoEnCarrito.cantidad++;
+    }else{
+        let producto = productos.find((producto) => producto.id === id);
+        carrito.push(producto)
+    }
 }
-verProductos()
+localStorage.getItem("carrito", JSON.stringify(carrito))
+
+//mostrar el carrito por pantalla 
+openCar.addEventListener("click", () => {
+    const carritoHeader = document.createElement("div");
+    carritoHeader.className = "carrito__header";
+    carritoHeader.innerHTML= `
+    <h1 class="carrito__titulo">carrito</h1>
+    <button class="carrito__close">X</button>
+    `
+    carritoContainer.append(carritoHeader)
+    
+    carrito.forEach((producto) => {
+        let carritoBody = document.createElement("div");
+        carritoBody.className = "carrito__body";
+        carritoBody.innerHTML = `
+        <img class="carrito__img" src="${producto.imagen}" alt="">
+        <h3 class="carrito__nombre">${producto.nombre}</h3>
+        <p class="cardcarrito__precio"> ${producto.precio} </p>
+        `;
+        carritoContainer.append(carritoBody)
+
+        let botonEliminar = document.createElement("button");
+        botonEliminar.className = "boton__eliminar";
+        botonEliminar.innerText = "eliminar"
+        carritoContainer.append(botonEliminar)
+        botonEliminar.addEventListener
+    //total a pagar 
+        const total = carrito.reduce((acc, item) => acc + item.precio, 0);
+
+        let totalCarrito = document.createElement("div")
+        totalCarrito.innerHTML = `<p>total a pagar: ${total}$</p>`
+        carritoContainer.append(totalCarrito)
+})
+// limpiar el carrito 
+let vaciarCarrito = document.createElement("button")
+vaciarCarrito.className = "boton__reset"
+vaciarCarrito.addEventListener("click", () => {
+    vaciarCarrito.textContent = "vaciar carrito"
+    localStorage.clear();
+})
+carritoContainer.append(vaciarCarrito)
+    })
+
+
+
+
+
+
